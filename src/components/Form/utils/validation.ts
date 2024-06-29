78fa6bebcc49b29285a97796f3e235456b03c68f
mokoko <guswlso2@naver.com>
2024-06-30 01:08:48 +0900

    fix: 비교 문자열 수정

--- src/components/Form/utils/validation.ts
+++ src/components/Form/utils/validation.ts
@@ -4,16 +4,13 @@ export const validateForm = (data: FormData): FormErrors => {
   const errors: FormErrors = {};
 
   Object.entries(data).forEach(([key, value]) => {
-    if (!value || value.trim().length === 0) {
-      errors[key] = '이 필드는 필수입니다.';
-    } else if (key === 'title' && value.length > 16) {
-      errors[key] = '제목은 최대 16자까지 입력할 수 있습니다';
-    }
-    if (key === 'title') {
+    if (key === 'todoTitle') {
       if (value.trim().length === 0) {
         errors[key] = '제목을 입력해 주세요.';
       } else if (value.length > 16) {
-        errors[key] = '제목은 최대 16자까지 입력할 수 있습니다';
+        errors[
+          key
+        ] = `제목은 최대 16자까지 입력할 수 있습니다. 현재: ${value.length}자리`;
       }
     }
 
@@ -23,7 +20,7 @@ export const validateForm = (data: FormData): FormErrors => {
       } else if (value.length > 80) {
         errors[
           key
-        ] = `내용은 최대 80자까지 입력할 수 있습니다 : ${value.length}`;
+        ] = `내용은 최대 80자까지 입력할 수 있습니다. 현재: ${value.length}자리`;
       }
     }
   });
